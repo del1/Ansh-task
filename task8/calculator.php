31016d81557ad33888f0d3e4cd02e32a1bc8a7df
test test <test@ansh.com>
2017-09-08 18:28:47 +0530

    task8 complete

---
+++ task8/calculator.php
@@ -0,0 +1,40 @@
+<?php 
+if(!empty($argv)) {
+	array_shift($argv);//filename
+	$function=array_shift($argv);//function name
+	if(function_exists($function))
+	{
+		$sum=call_user_func($function,$argv);
+		//only sending the 0th index as parameter , parameters enterd by hitting space wont be considered
+		echo $sum;
+	}else{
+		echo "Oops, Invalid Function name";
+	}
+}
+function add($argv) 
+{
+	$sum=0;
+	if(isset($argv[0])){
+		$argv=explode(",",$argv[0]);
+		foreach($argv as $key) {
+			if($key<=1000) {
+				$sum+=$key;
+			}
+		}
+	}
+	return $sum;
+}
+function multiply($argv) 
+{
+	$sum=1;
+	if(isset($argv[0])){
+		$argv=explode(",",$argv[0]);
+		foreach($argv as $key) {
+			//if($key>0) {
+				$sum*=$key;
+			//}
+		}
+	}
+	return $sum;
+}
+
