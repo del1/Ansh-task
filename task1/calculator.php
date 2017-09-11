2fcc6a3b0e520665438ebc925505f4f174d5d229
del1 <del1@usa.com>
2017-09-11 06:36:46 +0530

    updated calculator

--- task1/calculator.php
+++ task1/calculator.php
@@ -1,26 +1,46 @@
-<?php 
+<?php
+class Calculator
+{
+	private $Total;
+	function __construct()
+	{
+
+	}
+	public function sum($argv)
+	{
+		$this->Total=0;
+		if(isset($argv[0])){
+		$argv=explode(",",$argv[0]);
+			foreach($argv as $key) {
+				if($key>0) {//same instruction cost if we add 0
+					$this->Total+=$key;
+				}
+			}
+		}
+		return $this->Total;
+	}
+	public function multiplication($argv)
+	{
+		$this->Total=1;
+		if(isset($argv[0])){
+		$argv=explode(",",$argv[0]);
+			foreach($argv as $key) {
+				//if($key>0) {
+					$this->Total*=$key;
+				//}
+			}
+		}
+		return $this->Total;
+	}
+}
 if(!empty($argv)) {
 	array_shift($argv);//filename
 	$function=array_shift($argv);//function name
-	if(function_exists($function))
+	$calc= new Calculator();
+	if(method_exists($calc, $function))
 	{
-		$sum=call_user_func($function,$argv);
-		//only sending the 0th index as parameter , parameters enterd by hitting space wont be considered
-		echo $sum;
+		echo $calc->$function($argv)."\n";
 	}else{
-		echo "Oops, Invalid Function name";
+		echo "\n"."Oops, Invalid Function name"."\n";
 	}
-}
-function sum($argv) 
-{
-	$sum=0;
-	if(isset($argv[0])){
-		$argv=explode(",",$argv[0]);
-		foreach($argv as $key) {
-			if($key>0) {
-				$sum+=$key;
-			}
-		}
-	}
-	return $sum;
 }
---
+++ task1/calculator_old.php
@@ -0,0 +1,26 @@
+<?php 
+if(!empty($argv)) {
+	array_shift($argv);//filename
+	$function=array_shift($argv);//function name
+	if(function_exists($function))
+	{
+		$sum=call_user_func($function,$argv);
+		//only sending the 0th index as parameter , parameters enterd by hitting space wont be considered
+		echo $sum."\n";
+	}else{
+		echo "Oops, Invalid Function name";
+	}
+}
+function sum($argv) 
+{
+	$sum=0;
+	if(isset($argv[0])){
+		$argv=explode(",",$argv[0]);
+		foreach($argv as $key) {
+			if($key>0) {
+				$sum+=$key;
+			}
+		}
+	}
+	return $sum;
+}
